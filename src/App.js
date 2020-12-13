46a33bc629e5c20a13004305427f37f0ce9cda26
neilkrichi <neilkrichi@gmail.com>
2020-12-13 03:05:09 -0800

    React wildfire tracker initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from 'react'
+import Map from './Components/Map'
+import Loader from './Components/Loader'
+import Header from './Components/Header'
+
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      { !loading ? <Map eventData={eventData} /> : <Loader /> }
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Wildfire Tracker (Powered by NASA 🚀)</h1>
+        </header>
+    )
+}
+
+export default Header
