e66a340379a6de0b2f16ad0365dba15379f299c9
neilkrichi <neilkrichi@gmail.com>
2020-12-22 21:45:02 -0800

    adding gh-pages and updating location info

--- src/Components/LocationInfoBox.js
+++ src/Components/LocationInfoBox.js
@@ -3,8 +3,7 @@ const LocationInfoBox = ({ info }) => {
         <div className="location-info">
             <h2>Event Location Info</h2>
             <ul>
-                <li>ID: <strong>{ info.id }</strong></li>
-                <li>TITLE: <strong>{ info.title }</strong></li>
+                <li><strong>{ info.title }</strong></li>
             </ul>
         </div>
     )
